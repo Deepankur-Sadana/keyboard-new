d87e70a6bf32ffb32c16e08f332b5ffec4b5333d
deepankur <writetodeepankursadana@gmail.com>
2017-02-02 15:37:20 +0530

    added input method service files for basic functioning of the keyboard

--- app/src/main/java/deepankur/com/keyboardapp/activities/MainActivity.java
+++ app/src/main/java/deepankur/com/keyboardapp/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package deepankur.com.keyboardapp;
+package deepankur.com.keyboardapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import deepankur.com.keyboardapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/deepankur/com/keyboardapp/services/BaseInputMothodService.java
@@ -0,0 +1,149 @@
+package deepankur.com.keyboardapp.services;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.media.AudioManager;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+import java.util.List;
+
+import deepankur.com.keyboardapp.R;
+
+/**
+ * Created by deepankur on 1/6/17.
+ */
+
+public class BaseInputMothodService extends InputMethodService
+         implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView kv;
+    private Keyboard keyboard;
+
+    private boolean caps = false;
+
+
+    @Override
+    public View onCreateInputView() {
+        View view = getLayoutInflater().inflate(R.layout.keyboard, null);
+        kv = (KeyboardView) view.findViewById(R.id.keyboardView);
+        keyboard = new Keyboard(this, R.xml.qwerty);
+        kv.setKeyboard(keyboard);
+        kv.setOnKeyboardActionListener(this);
+        return view;
+    }
+
+
+    private void changeBrightness(int changeBrightnessByThisMuch) {
+        float curBrightnessValue = 0;
+        try {
+            curBrightnessValue = android.provider.Settings.System.getInt(
+                    getContentResolver(),
+                    android.provider.Settings.System.SCREEN_BRIGHTNESS);
+        } catch (SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int screen_brightness = (int) curBrightnessValue;
+        android.provider.Settings.System.putInt(getContentResolver(),
+                android.provider.Settings.System.SCREEN_BRIGHTNESS, screen_brightness + changeBrightnessByThisMuch);
+    }
+
+    private void playClick(int keyCode) {
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        switch (keyCode) {
+            case 32:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+                break;
+            case Keyboard.KEYCODE_DONE:
+            case 10:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case Keyboard.KEYCODE_DELETE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+                break;
+            default:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+        }
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+        playClick(primaryCode);
+        switch (primaryCode) {
+            case Keyboard.KEYCODE_DELETE:
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                caps = !caps;
+                keyboard.setShifted(caps);
+                kv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char) primaryCode;
+                if (Character.isLetter(code) && caps) {
+                    code = Character.toUpperCase(code);
+                }
+                ic.commitText(String.valueOf(code), 1);
+        }
+    }
+
+    final String TAG = getClass().getSimpleName();
+
+    void getAllPackages() {
+        final PackageManager pm = getPackageManager();
+        //get a list of installed apps.
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo packageInfo : packages) {
+            Log.d(TAG, "Installed package :" + packageInfo.packageName);
+            Log.d(TAG, "Source dir : " + packageInfo.sourceDir);
+            Log.d(TAG, "Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+        }
+        // the getLaunchIntentForPackage returns an intent that you can use with startActivity()
+    }
+
+    @Override
+    public void onPress(int primaryCode) {
+        Log.d(TAG, "onPress: "+primaryCode);
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+        Log.d(TAG, "onRelease: "+primaryCode);
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+        Log.d(TAG, "onText: "+text);
+    }
+
+    @Override
+    public void swipeDown() {
+        Log.d(TAG, "swipeDown: ");
+    }
+
+    @Override
+    public void swipeLeft() {
+        Log.d(TAG, "swipeLeft: ");
+    }
+
+    @Override
+    public void swipeRight() {
+        Log.d(TAG, "swipeRight: ");
+    }
+
+    @Override
+    public void swipeUp() {
+    }
+}
